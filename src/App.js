d787e0a5120aa6154e49398cbb2cbac15ee03d90
kiran <gandikotakiran99@gmail.com>
2021-12-22 10:41:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { StyledButton } from './components/button/Button.styles';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <button>Button</button> */}
+      <StyledButton>Styled Button</StyledButton>
+    </div>
+  );
+}
+
+export default App;
