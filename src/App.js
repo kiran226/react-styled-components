c2fda041177b91be286f69a61739a40b5d91a374
kiran <gandikotakiran99@gmail.com>
2021-12-22 11:03:09 +0530

    extending styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { StyledButton } from './components/button/Button.styles';
+import { FancyButton, StyledButton } from './components/button/Button.styles';
 import './App.css';
 
 
@@ -8,10 +8,17 @@ function App() {
     <div className="App">
       {/* <button>Button</button> */}
       <StyledButton>Styled Button</StyledButton>
+
       <div>
         <br/>
       </div>
+
       <StyledButton variant='outline'>Styled Button</StyledButton>
+
+      <div>
+        <br/>
+      </div>
+      <FancyButton as="a">Fancy Button</FancyButton>
     </div>
   );
 }
--- src/components/button/Button.styles.js
+++ src/components/button/Button.styles.js
@@ -14,3 +14,8 @@ font-size:16px;
 cursor: pointer;
 transition: 0.5s all ease-out
 `
+
+export const FancyButton =styled(StyledButton)`
+background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
+border:none;
+`
